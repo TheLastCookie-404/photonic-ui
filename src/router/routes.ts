e86e79b899fc4654ec2b25fe64aa72bac1ecf57d
The Last Cookie <74693705+TheLastCookie-404@users.noreply.github.com>
2025-05-17 01:11:44 +0500

    vue router added

---
+++ src/router/routes.ts
@@ -0,0 +1,9 @@
+import Home from "@/pages/Home.vue";
+import Test from "@/pages/Test.vue";
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/test", component: Test },
+];
+
+export default routes;
